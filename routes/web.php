a69e7cde0f78c58a4de7e7bbb9ce454379cb920e
mintaru <timethiofa@gmail.com>
2026-01-20 23:22:35 +0700

    30

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,6 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/1', function () {
-    return view('test');
-});
+
 
 Route::post('/send-form', [ContactController::class, 'sendForm']);
